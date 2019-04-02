3af6b11726eef47b09faf9ca97ff0c337349fca6
SamirWardak <wardag.as@yahoo.com>
2019-04-02 20:37:05 +0300

    Finish

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Target
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.fToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ptToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1335, 998);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 40);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1335, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(199, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 50);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Generate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 43);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(193, 50);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.fToolStripMenuItem,
+            this.sToolStripMenuItem,
+            this.tToolStripMenuItem,
+            this.chToolStripMenuItem,
+            this.ptToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1335, 40);
+            this.menuStrip2.TabIndex = 11;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // fToolStripMenuItem
+            // 
+            this.fToolStripMenuItem.Name = "fToolStripMenuItem";
+            this.fToolStripMenuItem.Size = new System.Drawing.Size(12, 36);
+            // 
+            // sToolStripMenuItem
+            // 
+            this.sToolStripMenuItem.Name = "sToolStripMenuItem";
+            this.sToolStripMenuItem.Size = new System.Drawing.Size(12, 36);
+            // 
+            // tToolStripMenuItem
+            // 
+            this.tToolStripMenuItem.Name = "tToolStripMenuItem";
+            this.tToolStripMenuItem.Size = new System.Drawing.Size(12, 36);
+            // 
+            // chToolStripMenuItem
+            // 
+            this.chToolStripMenuItem.Name = "chToolStripMenuItem";
+            this.chToolStripMenuItem.Size = new System.Drawing.Size(12, 28);
+            // 
+            // ptToolStripMenuItem
+            // 
+            this.ptToolStripMenuItem.Name = "ptToolStripMenuItem";
+            this.ptToolStripMenuItem.Size = new System.Drawing.Size(12, 28);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(75, 36);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.StartToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1335, 1062);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Мишень";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem fToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ptToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace Target
+{
+    public partial class Form1 : Form
+    {
+        private float radius = 100;
+        private int count;
+        private int counts;
+        bool starts = false;
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.Visible = false;
+            button2.Visible = false;
+        }
+
+        private void PaintCircle(int x, int y)
+        {
+            Graphics grf = this.CreateGraphics();
+
+            Graphics gr = pictureBox1.CreateGraphics();
+            gr.DrawEllipse(new Pen(Brushes.Red, 5), x, y, 3,3);
+    }
+
+
+        private void Panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.DrawToBitmap(bmp, pictureBox1.ClientRectangle);
+            //bmp.Save("D:\\testBmp111.bmp", ImageFormat.Bmp);
+            Color color = bmp.GetPixel(e.X, e.Y);
+
+            
+
+
+            if ((color.R.ToString() == "211" && color.G.ToString() == "211" && color.B.ToString() == "211")
+                || (color.R.ToString() == "255" && color.G.ToString() == "0" && color.B.ToString() == "0")) {
+                fToolStripMenuItem.Text = $"Попаданий: {++count}";
+            }
+            else
+            {
+                sToolStripMenuItem.Text = $"Промахи: {++counts}";
+            }
+            chToolStripMenuItem.Text = $"X: {e.X}";
+            ptToolStripMenuItem.Text = $"Y: {e.Y}";
+            tToolStripMenuItem.Text = $"Всего выстрелов: {count+ counts}";
+            PaintCircle(e.X, e.Y);
+
+            
+
+
+        }
+
+
+        private void Button2_Click(object sender, System.EventArgs v)
+        {
+
+            int res;
+            bool isInt = System.Int32.TryParse(richTextBox1.Text, out res);
+            if (starts)
+            {
+                if (isInt)
+                {
+                    if (float.Parse(richTextBox1.Text) >= pictureBox1.Width / 4 || float.Parse(richTextBox1.Text) < 10)
+                    {
+                        MessageBox.Show("Radius details error!!!", "Eror");
+                    }
+                    else
+                    {
+                        radius = float.Parse(richTextBox1.Text);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter radisu in diapazon 100-250", "Error!!!");
+                    return;
+                }
+                pictureBox1.MouseClick += Panel1_MouseClick;
+                chToolStripMenuItem.Text = "";
+                ptToolStripMenuItem.Text = "";
+                fToolStripMenuItem.Text = "Попаданий 0";
+                sToolStripMenuItem.Text = "Промахи 0";
+                tToolStripMenuItem.Text = "Всего выстрелов 0";
+                pictureBox1.Refresh();
+            }
+
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (radius != 0 && starts == true)
+            {
+                e.Graphics.TranslateTransform(pictureBox1.Width / 2f, pictureBox1.Height / 2f);
+                e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+
+                using (var brush = new SolidBrush(Color.LightGray))
+                {
+                    e.Graphics.FillPie(brush, -radius, -radius, radius * 2, radius * 2, -90, 90);
+                    e.Graphics.FillPolygon(brush, new PointF[] {
+                    new PointF(0, 0),
+                    new PointF(0, radius),
+                    new PointF(-radius, 0)
+                    });
+                }
+
+                using (var pen = new Pen(Color.Black) { CustomEndCap = new System.Drawing.Drawing2D.AdjustableArrowCap(2, 10, true) })
+                {
+                    var w = pictureBox1.Width / 2f;
+                    var h = pictureBox1.Height / 2f;
+                    e.Graphics.DrawLine(pen, -w, 0, w, 0);
+                    e.Graphics.DrawLine(pen, 0, h, 0, -h);
+                }
+            }
+            
+        }
+
+        private void StartToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            richTextBox1.Visible = true;
+            button2.Visible = true;
+            starts = true;
+            startToolStripMenuItem.Visible = false;
+        }
+
+        private void Form1_Resize(object sender, System.EventArgs e)
+        {
+            Invalidate();
+        }
+    }
+}
